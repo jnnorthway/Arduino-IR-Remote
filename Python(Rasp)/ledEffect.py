7d62ded253417d8aa2907094b25c7b809c3e9ab7
jnnorthway <jn.northway@gmail.com>
2019-06-24 20:36:19 -0700

    added seperate file for effects

---
+++ Python(Rasp)/ledEffect.py
@@ -0,0 +1,32 @@
+import pigpio
+pi = pigpio.pi()
+
+pin = [17,22,27]
+eff = 0
+boolLoop = True
+li = [[80,0,80 , 0,255,255 , 80,0,80],[255,0,0 , 0,255,0 , 0,0,255 , 255,255,0 , 80,0,80 , 0,255,255]]
+
+while boolLoop
+    red = li[eff][i%len(li[eff])]
+    green = li[eff][(i+1)%len(li[eff])]
+    blue = li[eff][(i+2)%len(li[eff])]
+    i+=3
+    while ( r != red or g != green or b != blue ):
+        if ( r < red ):
+            r += 1
+        if ( r > red ):
+            r -= 1
+
+        if ( g < green ):
+            g += 1
+        if ( g > green ):
+            g -= 1
+
+        if ( b < blue ):
+            b += 1
+        if ( b > blue ):
+            b -= 1
+
+        pi.set_PWM_dutycycle(pin[0], r)
+        pi.set_PWM_dutycycle(pin[1], g)
+        pi.set_PWM_dutycycle(pin[2], b)
