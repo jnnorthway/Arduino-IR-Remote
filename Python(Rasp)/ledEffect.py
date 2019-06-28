f4a89bb19b76e047ecad9aabe7dcd80b0b45ded1
jnnorthway <jn.northway@gmail.com>
2019-06-27 19:12:55 -0700

    Effect text file

--- Python(Rasp)/ledEffect.py
+++ Python(Rasp)/ledEffect.py
@@ -4,9 +4,20 @@ import sys
 pi = pigpio.pi()
 
 pin = [17,22,27]
-boolLoop = True
-li = [[80,0,80 , 0,255,255 , 80,0,80],[255,0,0 , 0,255,0 , 0,0,255 , 255,255,0 , 80,0,80 , 0,255,255]]
-stop_threads = False
+f=open("guru99.txt", "r")
+if f.mode == 'r':
+    f1.readlines()
+    count = 0
+    for x in f1:
+        j=0
+        for element in x:
+            if element != ',':
+                li[count][j] += element
+            else:
+                j+=1
+        count += 1
+
+#li = [[80,0,80 , 0,255,255 , 80,0,80],[255,0,0 , 0,255,0 , 0,0,255 , 255,255,0 , 80,0,80 , 0,255,255]]
 
 def runEffect(eff):
     i = 0
@@ -14,9 +25,9 @@ def runEffect(eff):
     g = li[eff][1]
     b = li[eff][2]
     while True:
-        red = li[eff][i%len(li[eff])]
-        green = li[eff][(i+1)%len(li[eff])]
-        blue = li[eff][(i+2)%len(li[eff])]
+        red = int(li[eff][i%len(li[eff])])
+        green = int(li[eff][(i+1)%len(li[eff])])
+        blue = int(li[eff][(i+2)%len(li[eff])])
         i+=3
         while ( r != red or g != green or b != blue ):
             if ( r < red ):
